9ad35b2daf642d08a49dbac4689be9e912e293fd
noname <vkoza035@uottawa.ca>
2020-04-14 12:22:03 -0400

    it asked me to

--- src/context/alerts/AlertState.js
+++ src/context/alerts/AlertState.js
@@ -1,26 +1,26 @@
 import React, {useReducer} from 'react'
-import {AlertContext} from "./alertContext";
-import {alertReducer} from "./alertReducer";
-import {HIDE_ALERT, SHOW_ALERT} from "../types";
+import {AlertContext} from './alertContext'
+import {alertReducer} from './alertReducer'
+import {HIDE_ALERT, SHOW_ALERT} from '../types'
 
-export const AlertState = ({childer}) => {
-    const [state, dispatch] = useReducer(alertReducer, {visible:false})
+export const AlertState = ({children}) => {
+    const [state, dispatch] = useReducer(alertReducer, {visible: false})
 
-    const show = (text, type='warning') => {
+    const show = (text, type = 'warning') => {
         dispatch({
             type: SHOW_ALERT,
             payload: {text, type}
         })
     }
 
-    const hide = () => dispatch({type:HIDE_ALERT})
+    const hide = () => dispatch({type: HIDE_ALERT})
 
     return (
         <AlertContext.Provider value={{
             show, hide,
             alert: state
         }}>
-            {childer}
+            {children}
         </AlertContext.Provider>
     )
 }
